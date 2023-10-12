2604b88e1da0df1b1447d79b49671c9c17f2452e
Dokyun Kim <donny5557@gmail.com>
2023-10-12 16:59:53 -0400

    Serial interface for speed control

--- IR_sensor/src/IR_sensor/IR_sensor.cpp
+++ IR_sensor/src/IR_sensor/IR_sensor.cpp
@@ -12,8 +12,8 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *motorL = AFMS.getMotor(4);
 Adafruit_DCMotor *motorR = AFMS.getMotor(3);
 
-// Car direction (1 for CW, -1 for CCW)
-const int direction = 1;
+// Store direction
+int direction = 0;
 
 // Define pins
 const int IR_L_READ = A2;
@@ -25,8 +25,8 @@ const int IR_M_OUT = 4;
 const int IR_R_OUT = 3;
 
 // Other constants
-const int linspeed = 25;
-const int turnspeed_default = 30;
+int linspeed = 25;
+int turnspeed_default = 30;
 const int min_diff = 200;
 
 // Define threshold value for black tape reading
@@ -64,22 +64,6 @@ void goRightInPlace(int speed)
   motorR->setSpeed(speed);
 }
 
-void goLeft()
-{
-  motorR->run(FORWARD);
-
-  motorR->setSpeed(turnspeed_default);
-  motorL->setSpeed(0);
-}
-
-void goRight()
-{
-  motorL->run(FORWARD);
-
-  motorL->setSpeed(turnspeed_default);
-  motorR->setSpeed(0);
-}
-
 void goStraight()
 {
   motorL->run(FORWARD);
@@ -89,9 +73,8 @@ void goStraight()
   motorR->setSpeed(linspeed);
 }
 
-void decideDirection(int IR_L, int IR_M, int IR_R)
+void decideDirection(int IR_L, int IR_M, int IR_R, int direction)
 {
-
   if ((IR_M > IR_L) & (IR_M > IR_R))
   {
     goStraight();
@@ -110,11 +93,11 @@ void decideDirection(int IR_L, int IR_M, int IR_R)
   {
     if (direction == 1)
     {
-      goRightInPlace(50);
+      goRightInPlace(turnspeed_default);
     }
     else
     {
-      goLeftInPlace(50);
+      goLeftInPlace(turnspeed_default);
     }
     delay(100);
   }
@@ -123,6 +106,7 @@ void decideDirection(int IR_L, int IR_M, int IR_R)
 void setup()
 {
   Serial.begin(9600); // set up Serial library at 9600 bps
+  Serial.setTimeout(100);
 
   if (!AFMS.begin())
   {
@@ -148,17 +132,48 @@ void setup()
   pinMode(IR_R_OUT, OUTPUT);
 }
 
+void inputSpeed()
+{
+
+  int speedMode = Serial.parseInt();
+  switch (speedMode)
+  {
+  case 0:
+    linspeed = 25;
+    break;
+
+  case 1:
+    linspeed = 30;
+    break;
+
+  case 2:
+    linspeed = 40;
+    break;
+
+  case 3:
+    linspeed = 50;
+    break;
+  }
+  Serial.print("Change speed to: ");
+  Serial.println(linspeed);
+}
+
 void loop()
 {
+  if (Serial.available() > 0)
+  {
+    inputSpeed();
+  }
+
   int IR_L_value = getReading(IR_L_OUT, IR_L_READ);
   int IR_M_value = getReading(IR_M_OUT, IR_M_READ);
   int IR_R_value = getReading(IR_R_OUT, IR_R_READ);
-  Serial.print("Left: ");
-  Serial.println(IR_L_value);
-  Serial.print("Middle: ");
-  Serial.println(IR_M_value);
-  Serial.print("Right: ");
-  Serial.println(IR_R_value);
-  decideDirection(IR_L_value, IR_M_value, IR_R_value);
+  // Serial.print("Left: ");
+  // Serial.println(IR_L_value);
+  // Serial.print("Middle: ");
+  // Serial.println(IR_M_value);
+  // Serial.print("Right: ");
+  // Serial.println(IR_R_value);
+  decideDirection(IR_L_value, IR_M_value, IR_R_value, direction);
   delay(50);
 }
